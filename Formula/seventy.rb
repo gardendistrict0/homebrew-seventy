b096babcbef862b1eaf73bdd9686b86868e9bb52
庭 <gardendistrict0x0@outlook.com>
2025-03-30 14:54:34 +1100

    Linux support

--- Formula/seventy.rb
+++ Formula/seventy.rb
@@ -1,12 +1,21 @@
 class Seventy < Formula
   desc "Seventy programming language"
   homepage "https://github.com/gardendistrict0/seventy"
-  url "https://github.com/gardendistrict0/seventy/releases/download/v0.1.0-beta/seventy-mac"
-  sha256 "265c19a79ec79714b53ed349e93e93da5a2fca0b4a7674c1219fd23deb54b37e"
+
+  # macOS version
+  if OS.mac?
+    url "https://github.com/gardendistrict0/seventy/releases/download/v0.1.0-beta/seventy-mac"
+    sha256 "265c19a79ec79714b53ed349e93e93da5a2fca0b4a7674c1219fd23deb54b37e"
+  # Linux version
+  elsif OS.linux?
+    url "https://github.com/gardendistrict0/seventy/releases/download/v0.1.0-beta/seventy-linux"
+    sha256 "248b1cd616c2d77ec3e3b5d69a08b2b8c2cae649d9829d641b449ad032924409"
+  end
+
   license "zlib/libpng"
 
   def install
-    bin.install "seventy-mac" => "seventy"
+    bin.install "seventy-#{OS.mac? ? 'mac' : 'linux'}" => "seventy"
   end
 
   test do
